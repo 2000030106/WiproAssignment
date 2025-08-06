dbbc45ffe00838648b2f7b39bb86cbdbed791850
“Vitali <saisrujan295@gmail.com>
2025-08-06 23:06:26 +0530

    cloudapi another files

---
+++ cloudapiemployee/src/main/java/com/springcloudgatewayemp/SpringCloudEmployee/SpringCloudEmployeeApplication.java
@@ -0,0 +1,13 @@
+package com.springcloudgatewayemp.SpringCloudEmployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudEmployeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudEmployeeApplication.class, args);
+	}
+
+}
